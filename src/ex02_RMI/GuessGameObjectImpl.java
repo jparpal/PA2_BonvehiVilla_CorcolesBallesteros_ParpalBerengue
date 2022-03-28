2df9ea5aca1fd89edaaa8fcbe34e9b43b7e55e16
maripaz1997 <maripazcorcoles@gmail.com>
2022-03-28 16:54:32 +0200

    Ex2

--- src/ex02_RMI/GuessGameObjectImpl.java
+++ src/ex02_RMI/GuessGameObjectImpl.java
@@ -7,7 +7,12 @@ import java.rmi.server.*;
 import java.util.*;
 
 public class GuessGameObjectImpl extends UnicastRemoteObject implements GuessGameObject{
-	
+	private int ranNum;
+	private Random ran = new Random();
+	protected GuessGameObjectImpl() throws RemoteException {
+			
+	}
+
 	// launcher
 	public static void main (String [] args)  {
 		try {
@@ -18,6 +23,32 @@ public class GuessGameObjectImpl extends UnicastRemoteObject implements GuessGam
 			e.printStackTrace();
 		}
 		System.out.println("Guess service bound and running");
+	}
+	
+	public int startGame () throws RemoteException{
+		ranNum = ran.nextInt();
+		return ranNum;
+	}
+
+	public String check(int id, int number) throws RemoteException {
+		/*	FALTA: Throws an exception if the id is unknown		*/
+		if(number == ranNum) {
+			return "EQUAL";
+		}else if(number < ranNum) {
+			return "LOWER";
+		}else {
+			return "HIGHER";
+		} /*	FALTA: if invoked when the number has been guessed in the last attempt but reset has not been invoked, throws an exception.		*/
+	}
+
+	public String reset(int id) throws RemoteException {
+		/*	FALTA: Throws an exception if the id is unknown		*/
+		ranNum = ran.nextInt();
+		return Integer.toString(ranNum);
+	}
+
+	public String terminate(int id) throws RemoteException {
+		return "Number of attempts: " + attempts + "Number of guessed numbers: " + guessed;	
 	}
 	
 }
